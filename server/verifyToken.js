13df1e72e0501ca2d9f5eac81612efe64f3934c4
PraveenGPK1206 <138776415+PraveenGPK1206@users.noreply.github.com>
2023-08-15 00:04:57 +0530

    Update verifyToken.js

--- server/verifyToken.js
+++ server/verifyToken.js
@@ -3,8 +3,9 @@ import { createError } from "./error.js";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
+  
 
-   req.user = user;
-   next()
+   
+    next()
 
 };
