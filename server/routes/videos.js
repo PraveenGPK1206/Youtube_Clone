7114da57948ae38f553884825a916e134367b6c0
PraveenGPK1206 <138776415+PraveenGPK1206@users.noreply.github.com>
2023-08-16 00:56:16 +0530

    Update videos.js

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { addVideo, addView, deleteVideo, getByTag, getVideo, random, search, sub, trend, updateVideo } from "../controllers/video.js";
+import { addVideo,Upload, addView, deleteVideo, getByTag, getVideo, random, search, sub, trend, updateVideo } from "../controllers/video.js";
 import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
 //create a video
 router.post("/",verifyToken,addVideo);
+router.post("/upload",Upload);
 router.put("/:id", verifyToken, updateVideo);
 router.delete("/:id", verifyToken, deleteVideo);
 router.get("/find/:id", getVideo);
