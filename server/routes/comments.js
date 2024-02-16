08b29ac504cee55d79f3ee21862d17d00234dc82
PraveenGPK1206 <138776415+PraveenGPK1206@users.noreply.github.com>
2024-02-16 14:46:22 +0530

    Update comments.js

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -2,7 +2,7 @@ import express from "express";
 import { addComment, deleteComment, getComments } from "../controllers/comment.js";
 import {verifyToken} from "../verifyToken.js";
 const router=express.Router();
-router.post("/",verifyToken,addComment);
+router.post("/addComment",verifyToken,addComment);
 router.delete("/:id",verifyToken,deleteComment);
 router.get("/:videoId",getComments);
 
